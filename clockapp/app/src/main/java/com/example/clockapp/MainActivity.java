69fb2e5d9d93222077f154abd75e6110db83ee52
vuhalam2003 <deodihocdau@gmail.com>
2024-05-24 12:31:07 +0700

    initial commit

---
+++ clockapp/app/src/main/java/com/example/clockapp/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.clockapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+
+import com.example.clockapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item ->{
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.alarm){
+                replaceFragment(new AlarmFragment());
+            } else if (itemId == R.id.timer) {
+                replaceFragment(new TimerFragment());
+            } else if (itemId == R.id.stopwatch){
+                replaceFragment(new StopWatchFragment());
+            }
+
+
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+}
